654b88022921c03d4cd4995bcec7dbc498d6b002
TheGhossst <nandukrishnapkm97@gmail.com>
2024-04-21 10:55:11 +0530

    structre completo

--- my-evoting-app/app/login.tsx
+++ my-evoting-app/app/login.tsx
@@ -1,3 +1,5 @@
+'use client';  //ou're importing a component that needs useState. It only works in a Client Component but none of its parents are marked with "use client", so they're Server Components by default.
+
 import React, { useState } from 'react';
 import styles from '../styles/Login.module.css';
 
@@ -11,36 +13,47 @@ const Login: React.FC = () => {
       };
     
     const handleLogin = async () => {
-        
+        const isValidUid = uid === 'blah';
+
+        if (isValidUid) {
+            setPopupMessage('Successfully logged in');
+            setShowPopup(true);
+          } else {
+            setPopupMessage('Login failed');
+            setShowPopup(true);
+          }
     };
 
+    const closePopup = () => {
+        setShowPopup(false);
+    };
 
-return (
-    <div className={styles.container}>
-      <h1 className={styles.title}>E-Voting Login</h1>
-      <div className={styles.loginContainer}>
-        <input
-          type="text"
-          placeholder="Enter MBCET UID"
-          value={uid}
-          onChange={handleUidChange}
-          className={styles.input}
-        />
-        <button onClick={handleLogin} className={styles.button}>
-          Login
-        </button>
-      </div>
-      {showPopup && (
-        <div className={styles.popup}>
-          <div className={styles.popupContent}>
-            <span className={styles.popupMessage}>{popupMessage}</span>
-            <button onClick={closePopup} className={styles.popupButton}>
-              OK
+    return (
+        <div className={styles.container}>
+        <h1 className={styles.title}>E-Voting Login</h1>
+        <div className={styles.loginContainer}>
+            <input
+                type="text"
+                placeholder="Enter MBCET UID"
+                value={uid}
+                onChange={handleUidChange}
+                className={styles.input}
+            />
+            <button onClick={handleLogin} className={styles.button}>
+                Login
             </button>
-          </div>
         </div>
-      )}
-    </div>
+        {showPopup && (
+            <div className={styles.popup}>
+            <div className={styles.popupContent}>
+                <span className={styles.popupMessage}>{popupMessage}</span>
+                <button onClick={closePopup} className={styles.popupButton}>
+                    OK
+                </button>
+            </div>
+            </div>
+        )}
+        </div>
   );
 };
 
