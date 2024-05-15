8a7961977f58a5218347d632633bc433d4eeef04
TheGhossst <nandukrishnapkm97@gmail.com>
2024-05-15 20:42:39 +0530

    changes

--- my-evoting-app/app/navbar.tsx
+++ my-evoting-app/app/navbar.tsx
@@ -1,25 +1,31 @@
 'use client' // Navbar.tsx
 import React from 'react';
 import styles from '../styles/Navbar.module.css';
-import Logo from './logo'; 
+import Logo from './logo';
 
 const Navbar: React.FC = () => {
-  return (
-    <nav className={styles.navbar}>
-      <Logo /> {}
-      <ul className={styles.navLinks}>
-        <li>
-          <button className={styles.navButton}>Home</button>
-        </li>
-        <li>
-          <button className={styles.navButton}>About</button>
-        </li>
-        <li>
-          <button className={styles.navButton}>Contact</button>
-        </li>
-      </ul>
-    </nav>
-  );
+    return (
+        <nav className={styles.navbar}>
+            <Logo /> { }
+            <ul className={styles.navLinks}>
+                <li key="home">
+                    <button className={styles.navButton} role="button">
+                        Home
+                    </button>
+                </li>
+                <li key="about">
+                    <button className={styles.navButton} role="button">
+                        About
+                    </button>
+                </li>
+                <li key="contact">
+                    <button className={styles.navButton} role="button">
+                        Contact
+                    </button>
+                </li>
+            </ul>
+        </nav>
+    );
 };
 
 export default Navbar;
